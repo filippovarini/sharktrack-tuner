2cf3fbe8371cef7920f15a5e21a229ff800817f6
Filippo Varini <filippo.varini20@imperial.ac.uk>
2024-05-13 21:16:51 +0100

    calculate frames to extract

--- generate_initial_dataset.py
+++ generate_initial_dataset.py
@@ -20,28 +20,24 @@ def maxn_df_sanity_check(maxn_df: pd.DataFrame):
     
 
 # %%
-def calculate_inverted_species_occurrance_ratio(species_occurrance_ratio: pd.Series):
-    inverted_species_occurrance_ratio = 1 / species_occurrance_ratio
-    normalised_species_occurrance_ratio = inverted_species_occurrance_ratio / inverted_species_occurrance_ratio.sum()
-    return normalised_species_occurrance_ratio
+def invert_ratio(species_df):
+    # We want rarely occurring species to have a higher number of frames of representation
+    # to achieve data balance
+    species_df["inverted_ratio"] = 1 / species_df["species_ratio"]
+    species_df["inverted_ratio"] = species_df["inverted_ratio"] / species_df["inverted_ratio"].sum()
 
 def calculate_frames_extraction_ratio(maxn_df: pd.DataFrame):
     avg_frames_per_maxn = 20
     total_frames = len(maxn_df) * avg_frames_per_maxn
 
-    species_occurrance_ratio = maxn_df.value_counts("species") / len(maxn_df)
-    species_occurrance_ratio = calculate_frames_extraction_ratio(species_occurrance_ratio)
-    frames_per_species = species_occurrance_ratio * total_frames
-    print(frames_per_species.sum())
+    species_df = maxn_df.groupby(["species"]).agg(n=("species", "count"))
+    species_df = species_df.reset_index()
+    species_df["species_ratio"] = species_df["n"] / species_df["n"].sum()
+    invert_ratio(species_df)
+    species_df["frames_per_maxn"] = species_df["inverted_ratio"] * total_frames / species_df["n"]
     
-
-    
-
-    
-
-
-
-
+    maxn_df = pd.merge(maxn_df, species_df["species", "frames_per_maxn"], on='species', how='inner')
+    print(maxn_df)
 
 calculate_frames_extraction_ratio(pd.read_csv("maxn.csv"))
 # %%
