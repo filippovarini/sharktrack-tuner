171e954178bfe9badbe9e02463d9a7a8fe923b23
Filippo Varini <filippo.varini20@imperial.ac.uk>
2024-05-13 21:29:41 +0100

    extract frames

--- generate_initial_dataset.py
+++ generate_initial_dataset.py
@@ -13,11 +13,11 @@ def maxn_df_sanity_check(maxn_df: pd.DataFrame):
     sample_video_path = Path(maxn_df[0]["video_path"]) / maxn_df[0]["chapter_name"]
     sample_video_time = maxn_df[0]["time_seconds"]
     vidcap = seek_video(sample_video_path)
-    ret, _ = seek_video(sample_video_path, sample_video_time)
+    ret, _ = vidcap.read()
+    print(ret)
     assert ret, f"Can't read video {sample_video_path}"
 
-    assert not maxn_df.isnull(), "MaxN file can't contain null values"
-    
+    assert not maxn_df.isnull(), "MaxN file can't contain null values"  
 
 # %%
 def invert_ratio(species_df):
@@ -34,10 +34,50 @@ def calculate_frames_extraction_ratio(maxn_df: pd.DataFrame):
     species_df = species_df.reset_index()
     species_df["species_ratio"] = species_df["n"] / species_df["n"].sum()
     invert_ratio(species_df)
-    species_df["frames_per_maxn"] = species_df["inverted_ratio"] * total_frames / species_df["n"]
+    species_df["frames_per_maxn"] = (species_df["inverted_ratio"] * total_frames / species_df["n"]).astype(int)
     
-    maxn_df = pd.merge(maxn_df, species_df["species", "frames_per_maxn"], on='species', how='inner')
+    maxn_df = pd.merge(maxn_df, species_df[["species", "frames_per_maxn"]], on='species', how='inner')
     print(maxn_df)
 
-calculate_frames_extraction_ratio(pd.read_csv("maxn.csv"))
+#%%
+
+def extract_frames(maxn_df: pd.DataFrame, dataset_path: Path):
+    # extract frames given ratio
+    # saves csv mapping image id to video path, time and species 
+    maxn_df_sanity_check(maxn_df)
+    extraction_guide = calculate_frames_extraction_ratio(maxn_df)
+    extraction_log = []
+
+    for index, row in extraction_guide.iterrows():
+        video_path = Path(row["video_path"]) / row["chapter_name"]
+        time_start = max(0, row["time_seconds"] - row["frames_per_maxn"] // 2)
+        curr_time = time_start
+        vidcap = seek_video(video_path, time_start)
+
+        ret = True
+
+        while ret and curr_time - time_start < row["frames_per_maxn"]:
+            ret, frame = vidcap.read()
+
+            extraction_log.append({
+                "video_path": str(video_path),
+                "time": curr_time,
+                "species": row["species"],
+                "image_id": len(extraction_log)
+            })
+            cv2.imwrite(dataset_path / "images", frame)
+            curr_time += 1
+        
+
+    extraction_df = pd.DataFrame(extraction_log)
+    extraction_df.to_csv(dataset_path / "log.csv")
+
+    return True
+
+maxn_df = pd.read_csv("maxn_df.csv")
+dataset_path = Path("initial_dataset")
+extract_frames("maxn.csv", )
+
+
+
 # %%
